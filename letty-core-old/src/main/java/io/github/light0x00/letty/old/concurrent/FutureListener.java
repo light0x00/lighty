995e512e77aef82076ec8d730240a681867e41aa
light <light0x00@163.com>
2023-08-08 19:08:28 +0800

    chore: 删除旧版实现

--- letty-core-old/src/main/java/io/github/light0x00/letty/old/concurrent/FutureListener.java
+++
@@ -1,6 +0,0 @@
-package io.github.light0x00.letty.old.concurrent;
-
-
-public interface FutureListener<T> {
-    void operationComplete(ListenableFutureTask<T> futureTask);
-}
--- letty-core-old/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author light
- * @since 2023/3/23
- */
-module letty {
-    requires org.slf4j;
-    requires jsr305;
-    requires lombok;
-}
