07aa76290bd05e3a89b9617faed9d53c726d0c2f
light <light0x00@163.com>
2023-08-21 13:24:29 +0800

    chore: 补充单元测试-输出管道写无限循环问题

--- lighty-core/src/test/java/io/github/light0x00/lighty/core/PipelineTest.kt
+++ lighty-core/src/test/java/io/github/light0x00/lighty/core/PipelineTest.kt
@@ -5,9 +5,8 @@ import io.github.light0x00.lighty.core.eventloop.NioEventLoopGroup
 import io.github.light0x00.lighty.core.facade.ClientBootstrap
 import io.github.light0x00.lighty.core.facade.InitializingNioSocketChannel
 import io.github.light0x00.lighty.core.facade.ServerBootstrap
-import io.github.light0x00.lighty.core.handler.ChannelContext
-import io.github.light0x00.lighty.core.handler.InboundChannelHandlerAdapter
-import io.github.light0x00.lighty.core.handler.InboundPipeline
+import io.github.light0x00.lighty.core.handler.*
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import java.net.InetSocketAddress
@@ -20,16 +19,27 @@ import java.util.concurrent.atomic.AtomicInteger
  */
 class PipelineTest {
 
+    val group = NioEventLoopGroup(2)
+
+    @AfterEach
+    fun destroy() {
+        group.shutdown().sync()
+    }
+
+    private fun address(): InetSocketAddress {
+        return InetSocketAddress(9000)
+    }
+
     /**
      * 测试 pipeline 中每个环节对上游的通知机制
      */
     @Test
     fun testPipelineUpstreamFutureNotify() {
-        val group = NioEventLoopGroup(2)
-
         val notifyUpstreamTimes = AtomicInteger()
 
-        ServerBootstrap()
+        val address = address()
+
+        val serverChannel = ServerBootstrap()
             .group(group)
             .childInitializer { channel: InitializingNioSocketChannel ->
                 channel.pipeline().add(
@@ -53,18 +63,70 @@ class PipelineTest {
                     }
                 )
             }
-            .bind(InetSocketAddress(9000))
+            .bind(address).sync()
 
-        val conn = ClientBootstrap()
+        val channel = ClientBootstrap()
             .group(group)
-            .connect(InetSocketAddress(9000)).sync()
+            .connect(address).sync()
 
-        conn.writeAndFlush("hello".toByteArray(StandardCharsets.UTF_8)).sync()
-
-        conn.closedFuture().sync()
-
-        group.shutdown().sync()
+        channel.writeAndFlush("hello".toByteArray(StandardCharsets.UTF_8)).sync()
+        channel.closedFuture().sync()
+        serverChannel.close().sync()
 
         Assertions.assertEquals(2, notifyUpstreamTimes.get())
     }
+
+    /**
+     * 测试 outbound pipeline 中写操作造成无限循环问题
+     */
+    @Test
+    fun testOutboundPipelineWriteEndlessLoop() {
+        val address = address()
+
+        val loopTimes = AtomicInteger()
+
+        val serverChannel = ServerBootstrap()
+            .group(group)
+            .childInitializer() {
+                it.pipeline()
+                    .add(object : OutboundChannelHandlerAdapter() {
+                        override fun onWrite(context: ChannelContext, data: Any, pipeline: OutboundPipeline) {
+                            //invoke a write inside outbound pipeline
+                            context.channel().writeAndFlush(data).addListener(pipeline.upstreamFuture())
+                        }
+                    })
+                    .add(object : OutboundChannelHandlerAdapter() {
+                        override fun onConnected(context: ChannelContext) {
+                            //write only for triggering outbound pipeline
+                            context.channel().writeAndFlush("1".toByteArray(StandardCharsets.UTF_8))
+                                .addListener {
+                                    context.channel().close()
+                                }
+                        }
+
+                        override fun onWrite(context: ChannelContext, data: Any, pipeline: OutboundPipeline) {
+                            //denote that the data pass back the head of the outbound pipeline
+                            //this will cause a endless loop
+                            if (loopTimes.incrementAndGet() > 1) {
+                                context.channel().close()
+                                return
+                            }
+                            pipeline.next(data).addListener(pipeline.upstreamFuture())
+                        }
+                    })
+            }
+            .bind(address)
+            .sync()
+
+        val channel = ClientBootstrap()
+            .group(group)
+            .connect(address)
+            .sync()
+
+        channel.closedFuture().sync()
+        serverChannel.close()
+        Assertions.assertEquals(1, loopTimes.get(),"Write inside outbound pipeline cause an endless loop")
+    }
+
+
 }
